3b57b018f64d99d75322f800aa03ef98edb71cb0
Yoshio Terada <tyoshio2002@yahoo.co.jp>
2024-05-27 21:38:09 +0900

    modified
    modified

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ export default {
     };
   },
   async mounted() {
-    const { text } = await (await fetch("/api/message")).json();
+    const { text } = await (await fetch("/api/messages")).json();
     this.message = text;
   }
 };
